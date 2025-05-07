1b9bfb127ad58b498d688c4533f85f7dbfbc349f
Lacciman11 <nwabekeyiprecious@gmail.com>
2025-05-07 13:40:42 +0100

    Initial commit

---
+++ src/data/user_info.js
@@ -0,0 +1,114 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Nwabekeyi Favour Somtochukwu",
+        description: "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Full-Stack Developer",
+        photo: "../me.jpg",
+        email: "nwabekeyiprecious@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        twitter: "https://twitter.com/NwabekeyiFavour",
+		github: "https://github.com/Lacciman11",
+		linkedin: "https://www.linkedin.com/in/nwabekeyi-precious-50439a348/",
+		instagram: "https://www.instagram.com/favourlacci/",
+		facebook: "https://facebook.com/profile.php?id=100066890213482",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "Pseudocode to C",
+			description: "Pseudocode to C is a web app that translates pseudocode into C code, aiding in the transition from high-level logic to a lower-level language.",
+            technologies: "React, Tailwind, Flask, SpaCy",
+			github: "https://github.com/dsbalico/pseudocode-to-c",
+            link: "https://pseudocode-to-c.netlify.app/"
+		},
+
+        {
+			title: "YouShare",
+			description: "Developed to learn full-stack web development using Nuxt3, Express, and PostgreSQL in fulfillment of my internship requirements.",
+			technologies: "Nuxt, Tailwind, Express, PostgreSQL",
+			github: "https://github.com/dsbalico/YouShare",
+            link: "https://github.com/dsbalico/YouShare"
+		},
+
+        {
+			title: "Draw & Classify",
+			description: "Draw & Classify is a web application designed to test my machine learning model, which classifies drawn letters, digits, or shapes.",
+			technologies: "React, Tailwind, Tensorflow",
+			github: "https://github.com/dsbalico/letter-digit-shape-classification",
+            link: "https://letterdigitshape-classification.netlify.app/"
+		},
+
+		{
+			title: "Chatroom",
+			description: "Chatroom is a simple online chatting web application where you can create chat rooms and have a fun conversation with a group of people.",
+			technologies: "React, Tailwind, Firebase",
+			github: "https://github.com/dsbalico/chatroom",
+            link: "https://thechatroom.netlify.app/"
+		},
+
+		{
+			title: "Sudoku Solver",
+			description: "Sudoku Solver is a web application that can generate or solve sudoku problems using simulated annealing, an optimization algorithm.",
+			technologies: "React, Tailwind, TypeScript",
+			github: "https://github.com/dsbalico/sudoku-solver-using-simulated-annealing",
+            link: "https://sudoku-solver-sa.netlify.app/"
+		},
+
+		{
+			title: "Probability Pick",
+			description: "Probability Pick is a PCSO Lotto Number Generator that calculates the likelihood of selecting winning numbers based on historical draw data.",
+			technologies: "React, Tailwind, Flask",
+			github: "https://github.com/dsbalico/pcso-lotto-number-generator",
+            link: "https://probability-pick.netlify.app/"
+		},
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Technological University of the Philippines",
+            degree: "Bachelor of Science in Computer Science",
+            duration: "2020 - 2024",
+            image: "tup.webp"
+        },
+        {
+            school: "STI College",
+            degree: "ICT in Mobile App and Web Development",
+            duration: "2018 - 2020",
+            image: "sti.webp"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Full-Stack Developer Intern",
+            company: "Babtech Computers",
+            duration: "Oct 2024 - Till date",
+            image: "babtech.jpeg",
+            descriptions: [
+                "Completed an extensive training program in full-stack web development.",
+                "Actively contributed to the ongoing development of a web application.",
+                "Mentored by senior developers on best practices and code optimization.",
+            ]
+        }
+     
+    ],
+
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at nwabekeyiprecious@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2025 Lacci Dev Nexus. All Rights Reserved"
+}
+
+export default info

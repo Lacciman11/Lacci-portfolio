af0bde88f0006001593fa9c169dc6dd5330dec1e
Lacciman11 <nwabekeyiprecious@gmail.com>
2025-11-05 15:18:01 +0100

    editted 'Technologies I use section'

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,13 +1,11 @@
 import { BiLogoNetlify, BiLogoPostgresql, BiLogoTypescript } from "react-icons/bi";
-import { CgVercel } from "react-icons/cg";
 import { DiGithubAlt, DiMongodb } from "react-icons/di";
 import { FaDharmachakra} from "react-icons/fa";
 import { FaCss3Alt, FaGitAlt, FaHtml5, FaNodeJs, FaReact} from "react-icons/fa6";
 import { FaBootstrap } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { IoLogoFirebase } from "react-icons/io5";
-import { SiSwagger, SiExpress, SiPostman, SiTailwindcss, SiRedis, SiRender} from "react-icons/si";
-import { IoCloudUploadSharp } from "react-icons/io5";
+import { SiSwagger, SiExpress, SiPostman, SiTailwindcss} from "react-icons/si";
 function Skills() {
   return (
     <section id="skills" className="mx-4 lg:mx-20">
@@ -19,6 +17,9 @@ function Skills() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 mt-8">
 				<span className="inline-flex items-center justify-between gap-x-2 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
           <FaHtml5 className="text-2xl" /> HTML
+        </span>
+        <span className="inline-flex items-center justify-between gap-x-2 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+        <BiLogoTypescript /> Typescript
         </span>
 				<span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
           <FaCss3Alt className="text-2xl" /> CSS
@@ -45,16 +46,7 @@ function Skills() {
           <DiGithubAlt className="text-2xl" /> Github
         </span>
 				<span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
-          <BiLogoNetlify className="text-2xl" /> Netlify
-        </span>
-				<span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
-          <CgVercel className="text-2xl" /> Vercel
-        </span>
-				<span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
           <IoLogoFirebase className="text-2xl" /> Firebase
-        </span>
-				<span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
-          <SiPostman className="text-2xl" /> Postman
         </span>
 				<span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
           <SiSwagger className="text-2xl" /> Swagger
@@ -64,15 +56,6 @@ function Skills() {
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
         <FaDharmachakra className="text-2xl" /> Chakra ui
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
-        <IoCloudUploadSharp  className="text-2xl" /> Cloudinary
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
-        <SiRedis  className="text-2xl" /> Redis
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
-        <SiRender  className="text-2xl" /> Render
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
         <FaBootstrap  className="text-2xl" /> Bootstrap
